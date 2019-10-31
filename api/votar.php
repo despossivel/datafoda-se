0946a09d1ae851ba03c7fcf389f549215315521b
despossivel <mattbmoller@gmail.com>
2019-10-31 14:24:44 -0300

    hallowins

--- api/votar.php
+++ api/votar.php
@@ -4,9 +4,9 @@ include('../dbcon.php');
 
 $candidato = $_GET['candidato'];
 
-$mysqli->query("INSERT INTO votos (idCandidato, votos, ip)VALUES ('" . $candidato . "', 1, '" . $ip . "')");
+$mysqli->query("INSERT INTO votos (idCandidato, votos)VALUES ('" . $candidato . "', 1)");
 if ($mysqli->affected_rows == 1) {
-	$mysqli->query("INSERT INTO ip (userip)VALUES ('" . $ip . "')");
+	//$mysqli->query("INSERT INTO ip (userip)VALUES ('" . $ip . "')");
 	$json = array("mensage" => 1);
 } else {
 	$json = array("mensage" => 0);
--- dbcon.php
+++ dbcon.php
@@ -5,11 +5,11 @@
 //  $mysqli = new mysqli("143.208.11.38", "root", "Hannibalshaw123.", "datafodase","3306"); //,"13306"
 // $mysqli = new mysqli("localhost", "root", "root", "dataFodase","13306"); //,"13306"
 $mysqli = new \mysqli(
-  '192.168.3.19',
+  'localhost',
   'root',
-  'root',
+  'root123.',
   'dataFodase',
-  '13306'
+  '3306'
 );
  
 $mysqli->set_charset("utf8");
